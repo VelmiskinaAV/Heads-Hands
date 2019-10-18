c2102d2cdac2387a3919282d151910defd30f9c2
VelmiskinaAV <36147136+VelmiskinaAV@users.noreply.github.com>
2019-10-18 23:39:48 +0300

    TestProgram

---
+++ MaxInd/MaxInd/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace FindMaxInd
+{
+   public class MaxInd
+    {
+        public int FindMax(int[] array)
+        {
+            int maxInd, midValue;
+
+            if (array.Length == 0)
+            {
+                return 0;
+            }
+
+            if (array.Length == 1)
+            {
+                return 1;
+            }
+
+            if (array.Length == 2)
+            {
+                if (array[0] >= array[1])
+                {
+                    return 1;
+                }
+                else
+                {
+                    return 2;
+                }
+            }
+
+            midValue = array[0];
+            maxInd = 1;
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i] > midValue)
+                {
+                    midValue = array[i];
+                    maxInd = i + 1;
+                }
+            }
+            return maxInd;
+        }
+
+        static void Main(string[] args)
+        {
+                        
+        }
+    }
+}
---
+++ MaxInd/TestMaxInd/UnitTest1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using FindMaxInd;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestMaxInd
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Test1()
+        {
+            MaxInd el = new MaxInd();
+            int[] array = { 1, 4, 7, 3, 6, 3 };
+            Assert.AreEqual(el.FindMax(array), 3);
+        }
+
+        [TestMethod]
+        public void Test2()
+        {
+            MaxInd el = new MaxInd();
+            int[] array = { 0, 3, 5, 7, 2, 8, 6 };
+            Assert.AreEqual(el.FindMax(array), 6);
+        }
+
+        [TestMethod]
+        public void Test3()
+        {
+            MaxInd el = new MaxInd();
+            int[] array = { 0, 2, 2, 7, 2, 2, 6 };
+            Assert.AreEqual(el.FindMax(array), 4);
+        }
+
+        [TestMethod]
+        public void Test4()
+        {
+            MaxInd el = new MaxInd();
+            int[] array = { 4 };
+            Assert.AreEqual(el.FindMax(array), 1);
+        }
+
+        [TestMethod]
+        public void Test5()
+        {
+            MaxInd el = new MaxInd();
+            int[] array = { 5, 2 };
+            Assert.AreEqual(el.FindMax(array), 1);
+        }
+
+        [TestMethod]
+        public void Test6()
+        {
+            MaxInd el = new MaxInd();
+            int[] array = { 2, 2 };
+            Assert.AreEqual(el.FindMax(array), 1);
+        }
+
+        [TestMethod]
+        public void Test7()
+        {
+            MaxInd el = new MaxInd();
+            int[] array = { 2, 6 };
+            Assert.AreEqual(el.FindMax(array), 2);
+        }
+
+        [TestMethod]
+        public void Test8()
+        {
+            MaxInd el = new MaxInd();
+            int[] array = { 5, 2, 15 };
+            Assert.AreEqual(el.FindMax(array), 3);
+        }
+
+        [TestMethod]
+        public void Test9()
+        {
+            MaxInd el = new MaxInd();
+            int[] array = { };
+            Assert.AreEqual(el.FindMax(array), 0);
+        }
+
+        [TestMethod]
+        public void Test10()
+        {
+            MaxInd el = new MaxInd();
+            int[] array = { 3, 3, 3, 3, 3, 3 };
+            Assert.AreEqual(el.FindMax(array), 1);
+        }
+    }
+}
